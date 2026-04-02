a467f0ed9e6dc73bfff66e5454b849141e4aeb30
ActiveMoon <cornenrl05@gmail.com>
2026-04-02 12:06:01 +0200

    Update LineThread.py

--- Code/line_detection/LineThread.py
+++ Code/line_detection/LineThread.py
@@ -23,11 +23,11 @@ class LineThread(threading.Thread):
             frame = frame[self.roi1[0]:self.roi1[1], self.roi2[0]:self.roi2[1]]
             intersection, frame = self.detector.getIntersection(frame)
             self.latestFrame = frame
-            # if intersection[1] >= self.roi1[1]*0.6:
-                # if self.cam.camPos == "left":
-                #     self.controller.steer(15, "right")
-                # elif self.cam.camPos == "right":
-                #     self.controller.steer(15, "left")
+            if intersection[1] >= self.roi1[1]*0.6:
+                if self.cam.camPos == "left":
+                    self.controller.steer(15, "right")
+                elif self.cam.camPos == "right":
+                    self.controller.steer(15, "left")
             # else:
                 # self.controller.steer(0, "left")
             time.sleep(1/30)
