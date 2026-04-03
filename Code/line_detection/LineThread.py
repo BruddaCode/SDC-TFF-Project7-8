5f2ea1028cf0908a5ff321ce154b1988bbd07958
ActiveMoon <cornenrl05@gmail.com>
2026-04-03 17:31:18 +0200

    adaptive steering calculation implemented

--- Code/line_detection/LineThread.py
+++ Code/line_detection/LineThread.py
@@ -11,6 +11,7 @@ class LineThread(threading.Thread):
         self.latestFrame = None
         self.running = True
         self.controller = controller
+        self.steerRoi = (roi1[1]*0.3, roi1[1]*0.8)
 
     def stop(self):
         self.running = False
@@ -24,19 +25,20 @@ class LineThread(threading.Thread):
             intersection, frame = self.detector.getIntersection(frame)
             self.latestFrame = frame
             if intersection is not None:
-                if intersection[1] >= self.roi1[1]*0.6:
+                if self.steerRoi[0] <= intersection[1] <= self.steerRoi[1]:
+                    percentage = 100 * ((intersection[1] - self.steerRoi[0]) / (self.steerRoi[1] - self.steerRoi[0]))
                     if self.cam.camPos == "left":
-                        self.controller.steer(40, "right")
+                        self.controller.steer(percentage, "right")
                         print("steering right")
                     elif self.cam.camPos == "right":
-                        self.controller.steer(40, "left")
+                        self.controller.steer(percentage, "left")
                         print("steering left")
-                elif intersection[1] >= self.roi1[1]*0.7:
+                elif intersection[1] > self.steerRoi[1]:
                     if self.cam.camPos == "left":
-                        self.controller.steer(60, "right")
+                        self.controller.steer(100, "right")
                         print("steering right")
                     elif self.cam.camPos == "right":
-                        self.controller.steer(60, "left")
+                        self.controller.steer(100, "left")
                         print("steering left")
             time.sleep(1/30)
         self.cam.release()
